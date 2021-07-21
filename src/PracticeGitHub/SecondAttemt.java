4b236e5f4a63f56d050845a6ad008fcdab80ef33
maftuna99 <rajabovamaftuna10@gmail.com>
2021-07-20 18:35:02 -0700

    Upteded

---
+++ src/PracticeGitHub/SecondAttemt.java
@@ -0,0 +1,10 @@
+package PracticeGitHub;
+
+public class SecondAttemt {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Class");
+
+        System.out.println("Im from Batch 23");
+    }
+}
